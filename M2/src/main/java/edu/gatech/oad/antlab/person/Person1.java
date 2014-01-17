a5866d171c70038ab9aeb7a12188ba06b1465b25
Steven <sthomas85@gatech.edu>
2014-01-17 12:28:55 -0500

    Deleted useless.p1.txt Completed Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,10 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if (input.length() > 1) {
+			return input.substring(2,input.length()) + input.substring(0,2);
+		}
+		return input;
 	}
 	
 	/**
